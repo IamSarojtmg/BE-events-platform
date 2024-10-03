4bb46c52f6108d82af7240208a7115d4d89f3c9e
Saroj Tamang <sarojtmg992@gmail.com>
2024-10-03 20:25:08 +0100

    readme

--- index.js
+++ index.js
@@ -27,21 +27,21 @@ app.get('/', async(req,res)=>{
 
 app.get('/events', async (req, res) => {
     try {
-        const events = await Event.find(); // Fetch all events
-        res.status(200).json({ events }); // Send events as response
+        const events = await Event.find(); 
+        res.status(200).json({ events }); 
     } catch (error) {
-        console.error(error); // Log the error for debugging
-        res.status(500).json({ message: "Internal server error" }); // Send error response
+        console.error(error); 
+        res.status(500).json({ message: "Internal server error" }); 
     }
 });
 
 app.post('/events', async (req, res) => {
     try {
         const event = await Event.create(req.body);
-        res.status(200).json(event); // Send created event as response
+        res.status(200).json(event); 
     } catch (error) {
-        console.error(error); // Log the error for debugging
-        res.status(500).json({ message: error.message }); // Send error response
+        console.error(error); 
+        res.status(500).json({ message: error.message }); 
     }
 });
 
---
+++ readme.md
@@ -0,0 +1,2 @@
+This is the backend of the eventsplatform projcet
+It is hosted here - https://heroku-api-two-68fd319974e4.herokuapp.com/events
