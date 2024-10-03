6faa9f3a51c37487bf4c2ebf4c54e17111c39ef8
Saroj Tamang <sarojtmg992@gmail.com>
2024-10-03 14:23:20 +0100

    added /

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(express.json());
 
 app.listen(PORT, () => console.log('API running on port', PORT));
 
+app.get('/', async(req,res)=>{
+    try{
+        res.status(200).json('Welcome to events api')
+    }catch(error){
+        res.status(500).json({message:'error'})
+    }
+})
+
 app.get('/events', async (req, res) => {
     try {
         const events = await Event.find(); // Fetch all events
