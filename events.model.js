35a1ad55136fcdf5a7d1b8b0b8142940223b5954
Saroj Tamang <sarojtmg992@gmail.com>
2025-02-05 11:33:51 +0000

    added tags

--- events.model.js
+++ events.model.js
@@ -1,56 +1,65 @@
 import mongoose from "mongoose";
 
 const eventSchema = mongoose.Schema(
-    {
-        title: {
-          type: String,
-          required: [true, "enter product name"],
-        },
-        date: {
-          type: Date,
-          required: true,
-        },
-        time: {
-          type: String,
-          required: true,
-        },
-        location: {
-          type: String,
-          required: true,
-        },
-        price: {
-          type: Number,
-          required: true,
-        },
-        duration: {
-          type: Number,
-          required: true,
-        }
-      },
-      {
-        timestamps: true,
-      }
+  {
+    title: {
+      type: String,
+      required: [true, "enter product name"],
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    tags: {
+      type: String,
+      required: true,
+      trim: true, // Removes leading/trailing spaces
+    },
+    
+  },
+  {
+    timestamps: true,
+  }
 );
 
+eventSchema.pre("save", function (next) {
+  if (this.date) {
+    const utcString = this.date.toISOString();
+    const localDate = new Date(utcString);
+    const offset = localDate.getTimezoneOffset();
+    const offsetHours = Math.abs(Math.floor(offset / 60))
+      .toString()
+      .padStart(2, "0");
+    const offsetMinutes = Math.abs(offset % 60)
+      .toString()
+      .padStart(2, "0");
+    const sign = offset > 0 ? "-" : "+";
 
-eventSchema.pre('save', function (next) {
-    if (this.date) {
-        const utcString = this.date.toISOString(); 
-        const localDate = new Date(utcString); 
-        const offset = localDate.getTimezoneOffset(); 
-        const offsetHours = Math.abs(Math.floor(offset / 60)).toString().padStart(2, '0');
-        const offsetMinutes = Math.abs(offset % 60).toString().padStart(2, '0');
-        const sign = offset > 0 ? '-' : '+';
+    const formattedDate = `${localDate
+      .toISOString()
+      .slice(0, 19)}${sign}${offsetHours}:${offsetMinutes}`;
 
-
-        const formattedDate = `${localDate.toISOString().slice(0, 19)}${sign}${offsetHours}:${offsetMinutes}`;
-        
-
-        this.date = formattedDate; 
-    }
-    next();
+    this.date = formattedDate;
+  }
+  next();
 });
 
-const Event = mongoose.model("Event", eventSchema)
+const Event = mongoose.model("Event", eventSchema);
 
-export default Event
+export default Event;
--- index.js
+++ index.js
@@ -1,84 +1,67 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import mongoose from 'mongoose';
-import Event from './events.model.js';
+import express from "express";
+import cors from "cors";
+
+import Event from "./events.model.js";
+
+import "dotenv/config";
+import mongoose from "mongoose";
 
 const URI = process.env.MONGO_URI;
 const PORT = process.env.PORT;
 
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => console.log('API running on port', PORT));
+app.listen(PORT, () => console.log("API running on port", PORT));
 
-app.get('/', async(req,res)=>{
-    try{
-        res.status(200).json('Welcome to events api')
-    }catch(error){
-        res.status(500).json({message:'error'})
-    }
-})
-
-app.get('/events', async (req, res) => {
-    try {
-        const events = await Event.find(); 
-        res.status(200).json({ events }); 
-    } catch (error) {
-        console.error(error); 
-        res.status(500).json({ message: "Internal server error" }); 
-    }
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json("Welcome to events api");
+  } catch (error) {
+    res.status(500).json({ message: "error" });
+  }
 });
 
-app.post('/events', async (req, res) => {
-    try {
-        const event = await Event.create(req.body);
-        res.status(200).json(event); 
-    } catch (error) {
-        console.error(error); 
-        res.status(500).json({ message: error.message }); 
-    }
+app.get("/events", async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.status(200).json({ events });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
-app.delete('/events/:id', async(req, res)=>{
-    try{
-        const {id} = req.params
-        const event = await Event.findByIdAndDelete(id)
-        if (!event) {
-            return res.status(404).json({message: `${id} not found`})
-        }
-        res.status(200).json({message: `product with ${id} deleted`})
-    }catch(error){
-        res.status(500).json({message:error.message})
+app.post("/events", async (req, res) => {
+
+
+  try {
+    const event = await Event.create(req.body);
+    res.status(200).json(event);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/events/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const event = await Event.findByIdAndDelete(id);
+    if (!event) {
+      return res.status(404).json({ message: `${id} not found` });
     }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    res.status(200).json({ message: `product with ${id} deleted` });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 // const client = new MongoClient(URI)
 // const database = client.db('game-store')
